170a2c39011b48d74e88e74838d33d4e6566adde
Darling-Amk <vibersteamplay@gmail.com>
2025-06-03 01:10:13 +0300

    Изменена админка на кастомную

--- blog/admin.py
+++ blog/admin.py
@@ -4,18 +4,31 @@ from .models import Category, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         "id",
         "title",
         "watched_count",
+        "category",
         "is_published",
         "created_at",
         "updated_at",
-    ]
+    )
+    list_display_links = ("id", "title")
+    list_editable = ("is_published",)
+    readonly_fields = ("watched_count",)
+    list_filter = (
+        "id",
+        "title",
+        "category",
+        "is_published",
+        "created_at",
+        "updated_at",
+    )
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["id", "title"]
+    list_display = ("id", "title")
+    list_display_links = ("id", "title")
 
 
 admin.site.register(Category, CategoryAdmin)
--- conf/settings.py
+++ conf/settings.py
@@ -16,13 +16,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig" #
+    "blog.apps.BlogConfig",  #
 ]
 
 MIDDLEWARE = [
